32e55dc1fc7b003395fcaaa6ddc0e0e9e567a6d7
tien-ngnvan <tien.ngnvan@gmail.com>
2025-02-19 20:10:59 +0700

    chore: setup file

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@
 
 import os
 from setuptools import setup
+from setuptools import find_packages
 
 VERSION = "0.0.1"
 
@@ -18,8 +19,12 @@ def get_requirements():
 if __name__ == "__main__":
     setup(
         name="jepa",
+        packages=find_packages(),
         version=VERSION,
         description="JEPA research code.",
+        long_description=get_readme_description(),
+        long_description_content_type='text/markdown',
         python_requires=">=3.9",
         install_requires=get_requirements(),
+        url='https://github.com/tien-ngnvan/jepa',
     )
